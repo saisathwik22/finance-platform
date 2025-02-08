cde76ed7cc6989ad734928ef1f74834f41ab9fc2
Sai Sathwik Reddy <reddysaisathwik022@gmail.com>
2025-02-08 19:28:02 +0530

    SignIn and SignUp through Clerk integrated using routes method. CodeStage - 32:00

--- app/layout.js
+++ app/layout.js
@@ -1,29 +1,32 @@
 import Header from "@/components/header";
 import "./globals.css";
 import { Inter } from "next/font/google";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "WalletWise",
+  title: "FundFlow",
   description: "Finance Platform ",
 };
 
 export default function RootLayout({ children }) {
   return (
-    <html lang='en'>
-      <body className={`${inter.className}`}>
-        {/* header */}
-        <Header />
-        <main className='min-h-screen'>{children}</main>
+    <ClerkProvider>
+      <html lang='en'>
+        <body className={`${inter.className}`}>
+          {/* header */}
+          <Header />
+          <main className='min-h-screen'>{children}</main>
 
-        {/* Footer */}
-        <footer className='bg-blue-50 py-12'>
-          <div className='container mx-auto px-4 text-center text-gray-600'>
-            <p>Made with passion</p>
-          </div>
-        </footer>
-      </body>
-    </html>
+          {/* Footer */}
+          <footer className='bg-blue-50 py-12'>
+            <div className='container mx-auto px-4 text-center text-gray-600'>
+              <p>Made with passion</p>
+            </div>
+          </footer>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
