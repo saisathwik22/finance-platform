3f86dd123e092d93b8553ad1f945c883b8d9f4c7
Sai Sathwik Reddy <reddysaisathwik022@gmail.com>
2025-02-12 09:20:47 +0530

    Logo, Header buttons added to Header Component. CodeStage - 40:40

--- app/page.js
+++ app/page.js
@@ -1,5 +1,9 @@
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
-  return <Button variant='destructive'>hello, gud mrng</Button>;
+  return (
+    <div className='mt-40'>
+      <Button variant='destructive'>hello, gud mrng</Button>;
+    </div>
+  );
 }
--- components/header.jsx
+++ components/header.jsx
@@ -1,14 +1,57 @@
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+import Image from "next/image";
+import Link from "next/link";
+import { Button } from "./ui/button";
+import { LayoutDashboard, PenBox } from "lucide-react";
 
 const Header = () => {
   return (
-    <div className='fixed top-0'>
-      <SignedOut>
-        <SignInButton />
-      </SignedOut>
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
+    <div className='fixed top-0 w-full bg-white/80 backdrop:blur-md z-50 border-b'>
+      <nav className='container mx-auto px-1 py-1 flex items-center justify-between'>
+        <Link href='/'>
+          <Image
+            src={"/logo.png"}
+            alt='fund flow logo'
+            height='30'
+            width='100'
+            className='h-13 w-30 object-contain'
+          />
+        </Link>
+
+        <div className='flex items-center space-x-4 '>
+          <SignedIn>
+            <Link
+              href={"/dashboard"}
+              className='text-gray-600 hover:text-blue-600 flex items-center gap-2'
+            >
+              <Button variant='outline'>
+                <LayoutDashboard size={18} />
+                <span className='hidden md:inline'>Dashboard</span>
+              </Button>
+            </Link>
+            <Link href={"/transaction/create"}>
+              <Button className='flex items-center gap-2'>
+                <PenBox size={18} />
+                <span className='hidden md:inline'>Add Transaction</span>
+              </Button>
+            </Link>
+          </SignedIn>
+          <SignedOut>
+            <SignInButton forceRedirectUrl='/dashboard'>
+              <Button variant='outline'>Login</Button>
+            </SignInButton>
+          </SignedOut>
+          <SignedIn>
+            <UserButton
+              appearance={{
+                elements: {
+                  avatarBox: "w-10 h-10",
+                },
+              }}
+            />
+          </SignedIn>
+        </div>
+      </nav>
     </div>
   );
 };
