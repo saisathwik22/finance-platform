38e3d0b1173d5789b3242529b9d32b2c844ac7f2
Sai Sathwik Reddy <reddysaisathwik022@gmail.com>
2025-03-02 12:51:53 +0530

    Rate limiting added using Arcjet. CodeStage - 5:00:00

--- actions/transaction.js
+++ actions/transaction.js
@@ -1,6 +1,8 @@
 "use server";
 
+import aj from "@/lib/arcjet";
 import { db } from "@/lib/prisma";
+import { request } from "@arcjet/next";
 import { auth } from "@clerk/nextjs/server";
 import { revalidatePath } from "next/cache";
 
@@ -15,6 +17,28 @@ export async function createTransaction(data) {
     if (!userId) throw new Error("Unauthorized");
 
     // Arcjet to add rate limiting
+    const req = await request();
+    // check rate limit
+    const decision = await aj.protect(req, {
+      userId,
+      requested: 1, // specify how many tokens to consume
+    });
+
+    if (decision.isDenied()) {
+      if (decision.reason.isRateLimit()) {
+        const { remaining, reset } = decision.reason;
+        console.error({
+          code: "RATE_LIMIT_EXCEEDED",
+          details: {
+            remaining,
+            resetInSeconds: reset,
+          },
+        });
+
+        throw new Error("Too many requests. Please try again later");
+      }
+      throw new Error("Request Blocked!");
+    }
 
     const user = await db.user.findUnique({
       where: { clerkUserId: userId },
